3f4506c480eea8872c32e79c6cb95c608addedb1
Kevin Meireles <105472882+Kebions2077@users.noreply.github.com>
2025-10-26 23:05:15 -0300

    Mudança na criptografia
    -Suporte a frases
    -Inverte o deslocamento
    -O codigo se repetia, adicionei uma função para isso nao acontecer
    -valida a entrada

--- ceasar.c
+++ ceasar.c
@@ -3,56 +3,57 @@
 #include <string.h>
 #include <ctype.h>
 
-int convertor(int cifra, char palavra[]);
-int main (int argc, char *argv[])
-{
-    if(argc != 2)
-    {
-        printf("digite o numero de casas puladas ao iniciar\n");
+#define MAX_TEXTO 256
+
+void cifra_cesar(char texto[], int deslocamento, int decifrar) {
+    int tamanho = strlen(texto);
+    if (decifrar) deslocamento = 26 - (deslocamento % 26);
+
+    for (int i = 0; i < tamanho; i++) {
+        char c = texto[i];
+
+        if (islower(c)) {
+            c = ((c - 'a' + deslocamento) % 26) + 'a';
+        } else if (isupper(c)) {
+            c = ((c - 'A' + deslocamento) % 26) + 'A';
+        }
+        texto[i] = c;
     }
-
-    int cifra = atoi(argv[1]);
-    char palavra[50];
-
-    printf("digite a palavra que deseja criptografar:\n");
-    scanf("%s", palavra);
-
-    convertor(cifra, palavra);
 }
-int convertor(int cifra, char palavra[])
-{
-    int index;
-    int texto;
-    int cont;
-    for(int i = 0; i < strlen(palavra); i++)
-    {
-        char letra = palavra[i];
-       
-        if(islower(letra))
-        {
-            index = letra - 'a';
 
-            texto = (index + cifra) % 26;
-            
-            texto += 'a';
-            printf("%c", texto);   
-        }
-        else if(isalpha(letra))
-        {
-           
-            index = letra - 'A';
-
-            texto = (index + cifra) % 26;
-            
-            texto += 'A';
-            printf("%c", texto);   
-        }
-        else
-        {
-            printf("%c", letra);
-        }
-        return 0;
+int validar_numero(char *str) {
+    for (int i = 0; str[i]; i++) {
+        if (!isdigit(str[i])) return 0;
     }
-    
-    
+    return 1;
+}
+
+int main(int argc, char *argv[]) {
+    if (argc != 2 || !validar_numero(argv[1])) {
+        printf("Uso correto: %s <numero de casas>\n", argv[0]);
+        return 1;
+    }
+
+    int deslocamento = atoi(argv[1]);
+    char texto[MAX_TEXTO];
+    int opcao;
+
+    printf("Digite o texto que deseja criptografar/decifrar:\n");
+    fgets(texto, MAX_TEXTO, stdin);
+    texto[strcspn(texto, "\n")] = '\0';
+
+    printf("Escolha a opção:\n1 - Criptografar\n2 - Decifrar\n");
+    scanf("%d", &opcao);
+
+    if (opcao == 1) {
+        cifra_cesar(texto, deslocamento, 0);
+        printf("Texto criptografado: %s\n", texto);
+    } else if (opcao == 2) {
+        cifra_cesar(texto, deslocamento, 1);
+        printf("Texto decifrado: %s\n", texto);
+    } else {
+        printf("Opção inválida!\n");
+    }
+
+    return 0;
 }
