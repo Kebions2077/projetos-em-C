5634922749465a5f27704bf5dee66c743c873afc
Kevin Meireles <105472882+Kebions2077@users.noreply.github.com>
2025-11-02 23:01:58 -0300

    dica

--- joguin.c
+++ joguin.c
@@ -1,42 +1,120 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 typedef struct 
 {
     int vida;
     int passos;
+    int x, y;
     char nickname[10];
-}status;
+} status;
 
-void mapa (status player[1]);
-int main (void)
-{
-    //pegando o nome do jogador
-    status player[1];
- 
-    printf("digite seu nome player: ");
-    fgets(player->nickname, sizeof(player->nickname), stdin);
-    mapa(player);
+void exibirMapa3D(status *player);
+void moverJogador(status *player, char direcao);
+
+void apaga() {
+    fflush(stdout);
+    #ifdef _WIN32
+        system("cls");
+    #else
+        system("clear");
+    #endif
 }
 
-void mapa (status player[1])
+int main(void)
 {
-    //criando o mapa *teste
-    //primeiro for é linha e o segundos as colunas
-    for(int i = 0; i < 5; i++)
+    status player;
+    player.vida = 100;
+    player.passos = 0;
+    player.x = 2;
+    player.y = 5;
+
+    printf("Digite seu nome player: ");
+    fgets(player.nickname, sizeof(player.nickname), stdin);
+    player.nickname[strcspn(player.nickname, "\n")] = '\0';
+
+    char comando;
+
+    printf("\nBem-vindo, %s! Use W, A, S, D para se mover. (Q para sair)\n", player.nickname);
+
+    while (1)
     {
-        for(int j = 1; j < 10; j++)
+        apaga();  // Limpa a tela antes de mostrar o mapa
+        exibirMapa3D(&player);
+        printf("\nVida: %d | Passos: %d\n", player.vida, player.passos);
+        printf("Digite seu movimento: ");
+        scanf(" %c", &comando);
+
+        if (comando == 'q' || comando == 'Q')
         {
-            if(i == 2 && j == 5)
-            {
-                printf( "%2s", "& ");
-            }
+            printf("\nSaindo do jogo... valeu, %s!\n", player.nickname);
+            printf("...\n..\n.\n");
+            break;
+        }
+
+        switch (comando)
+        {
+            case 'w': case 'W':
+            case 'a': case 'A':
+            case 's': case 'S':
+            case 'd': case 'D':
+                moverJogador(&player, comando);
+                break;
+            default:
+                printf("\nComando inválido! Use W, A, S, D ou Q.\n");
+                #ifdef _WIN32
+                    system("pause");  // Windows
+                #else
+                    printf("Pressione Enter para continuar...");
+                    getchar();  // consome o '\n' do scanf
+                    getchar();  // espera o Enter
+                #endif
+                break;
+        }
+    }
+
+    return 0;
+}
+
+void exibirMapa3D(status *player)
+{
+    printf("\n");
+    for (int i = 0; i < 5; i++)
+    {
+        // deslocamento lateral pra dar a sensação de profundidade
+        for (int k = 0; k < 5 - i; k++)
+            printf(" ");
+
+        for (int j = 0; j < 10; j++)
+        {
+            if (i == player->x && j == player->y)
+                printf("/&\\ ");  // jogador
             else
-            {
-                printf("%2s", "# ");
-            }
+                printf("/_\\ ");  // chão com relevo
         }
         printf("\n");
-        
     }
+    printf("\n");
+}
+
+void moverJogador(status *player, char direcao)
+{
+    switch (direcao)
+    {
+        case 'w': case 'W':
+            if (player->x > 0) player->x--;
+            break;
+        case 's': case 'S':
+            if (player->x < 4) player->x++;
+            break;
+        case 'a': case 'A':
+            if (player->y > 0) player->y--;
+            break;
+        case 'd': case 'D':
+            if (player->y < 9) player->y++;
+            break;
+    }
+
+    player->passos++;
 }
